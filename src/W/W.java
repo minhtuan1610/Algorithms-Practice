5560f416187a18ef08ab695f317214f29baa8909
minhtuan <tuannm1610@gmail.com>
2022-08-16 22:47:55 +0700

    c = 1 2

---
+++ src/W/W.java
@@ -0,0 +1,24 @@
+package W;
+
+public class W {
+    static int c = 0;
+
+    private W() {
+        System.out.println("c = " + c);
+    }
+
+    public static void main(String[] args) {
+        W w1 = c();
+        W w2 = c(w1);
+        W w3 = c(w2);
+        W w4 = c(w3);
+    }
+
+    static W c() {
+        return c++ <= 0 ? new W() : null;
+    }
+
+    static W c(W w) {
+        return w.c++ == 1 ? new W() : null;
+    }
+}
